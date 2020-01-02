c9eeb54d8c638c2efa1f3123c562fb29e86f6e0f
unknown <pchyo92@gmail.com>
2020-01-02 20:01:11 +0900

    add disabled

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,34 +19,41 @@ class Counter extends Component {
       onDelete
     } = this.props;
     return (
-      <div>
+      <React.Fragment>
         {children}
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => {
-            onIncrement(counter);
-          }}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            onDecrement(counter);
-          }}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Decrement
-        </button>
-        <button
-          onClick={() => {
-            onDelete(counter.id);
-          }}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
-      </div>
+        <div className="row">
+          <div className="col-1">
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          </div>
+          <div className="col">
+            <button
+              onClick={() => {
+                onIncrement(counter);
+              }}
+              className="btn btn-secondary btn-sm"
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                onDecrement(counter);
+              }}
+              className="btn btn-danger btn-sm m-2"
+              disabled={counter.value <= 0}
+            >
+              -
+            </button>
+            <button
+              onClick={() => {
+                onDelete(counter.id);
+              }}
+              className="btn btn-danger btn-sm"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 
